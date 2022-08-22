7caf2068fbdc0aa845428e90c8d93047e77fc733
aizijiCOD <aletv1501@gmail.com>
2022-08-22 12:51:37 -0400

    Add routes and basic structure for the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header';
+import Rockets from './pages/rockets';
+import User from './pages/user';
+import Missions from './pages/missions';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import logo from '../img/planet.png';
 
-const { Link } = require('react-router-dom');
+const { NavLink } = require('react-router-dom');
 
 const Header = () => (
   <header>
     <img src={logo} alt="logo" className="logo" />
     <ul>
-      <Link to="/">nav 1</Link>
-      <Link to="/">nav 2</Link>
+      <NavLink to="/">Rockets</NavLink>
+      <NavLink to="/missions">Missions</NavLink>
+      <NavLink to="/user">My Profile</NavLink>
     </ul>
   </header>
 );
