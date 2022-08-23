4b87d6cc32ce51fb08082a87f36e9b37ccb0b7de
lucascr <lcrodrigues1995@gmail.com>
2022-08-23 13:56:42 -0300

    add storeReducer as provider for the index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import storeReducer from './redux/configureStore';
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-  ,
+  <Provider store={storeReducer}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 );
