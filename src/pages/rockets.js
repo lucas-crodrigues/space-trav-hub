74412dcf42719b30e4cf02a16531adf46d968b48
lucascr <lcrodrigues1995@gmail.com>
2022-08-23 13:22:02 -0300

    Render Rockets

--- src/pages/rockets.js
+++ src/pages/rockets.js
@@ -1,7 +1,53 @@
-const Rockets = () => (
-  <>
-    <p>Rockets</p>
-  </>
-);
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import RocketList from '../components/Rockets/RocketList';
+import {
+  getRockets,
+  selectAllRockets,
+  getRocketsStatus,
+  getRocketsError,
+} from '../redux/rockets/fetchRockets';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+
+  const rocketList = useSelector(selectAllRockets);
+  const rocketsStatus = useSelector(getRocketsStatus);
+  const error = useSelector(getRocketsError);
+
+  React.useEffect(() => {
+    if (rocketsStatus === 'initial') {
+      dispatch(getRockets());
+    }
+  }, [rocketsStatus, dispatch]);
+
+  let content;
+
+  if (rocketsStatus === 'loading') {
+    content = <p>Loading...</p>;
+  } else if (rocketsStatus === 'succeed') {
+    // const keys = Object.keys(rocketList[0]);
+    // const library = [];
+    // keys.forEach((element) => {
+    //   library.push({
+    //     id: element,
+    //     author: rocketList[0][element][0].author,
+    //     title: rocketList[0][element][0].title,
+    //     category: rocketList[0][element][0].category,
+    //   });
+    // });
+    content = <RocketList rocketsList={rocketList} />;
+  } else if (rocketsStatus === 'fail') {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <>
+      <div className="rocketsArea">
+        {content}
+      </div>
+    </>
+  );
+};
 
 export default Rockets;
