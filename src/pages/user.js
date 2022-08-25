aa093d6b339b3b618939c01dbd71297cc6970ff2
lucascr <lcrodrigues1995@gmail.com>
2022-08-25 14:48:26 -0300

    Add user's rockets render

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,27 +1,42 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux/es/exports';
 import UserMissions from '../components/user/user-missions';
-import { userMissionsAction } from '../redux/user/user';
-import '../components/user/user-missions.css';
+import UserRockets from '../components/user/userRockets';
+import { userMissionsAction, userRocketsAction } from '../redux/user/user';
+import '../components/user/user.css';
 
 const User = () => {
   const userData = useSelector((state) => state.user);
   const missionsData = useSelector((state) => state.missions);
+  const rocketsData = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(userMissionsAction(missionsData));
+    dispatch(userRocketsAction(rocketsData));
   }, []);
   return (
     <>
-      <ul className="user-missions-ul">
-        <div className="user-missions-title"> My Missions</div>
-        {userData.missions.map((mission) => (
-          <UserMissions
-            key={mission.mission_id}
-            name={mission.mission_name}
-          />
-        ))}
-      </ul>
+      <div className="user-page">
+        <ul className="user-ul">
+          <div className="user-title"> My Missions</div>
+          {userData.missions.map((mission) => (
+            <UserMissions
+              key={mission.mission_id}
+              name={mission.mission_name}
+            />
+          ))}
+        </ul>
+        <div className="vertical-line"></div>
+        <ul className="user-ul">
+          <div className="user-title"> My Missions</div>
+          {userData.rockets.map((rocket) => (
+            <UserRockets
+              key={rocket.id}
+              name={rocket.rocket_name}
+            />
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
